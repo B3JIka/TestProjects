47f6c51923b66c22748aba1043bbee28a1d78b38
B3JIka <fiya.zima@mail.ru>
2022-07-04 14:12:37 +0300

    master

--- calculator.py
+++ calculator.py
@@ -5,17 +5,17 @@ if z=="+":
     result=x+y   
 elif z=="-":     
     result=x-y   
-elif z==pow:     
+elif z=='pow':
     result=pow(x,y)   
 elif z=="*":      
     result=x*y    
 elif y!=0:     
     if z=="/":       
         result=x/y     
-    elif z==div:        
+    elif z== 'div':
         result=x//y     
-    elif z==mod:        
-        result=x%y      
+    elif z=='mod':
+        result=x % y
 elif y==0:      
     result="Деление на 0!"   
 print("Результат вычислений =",result)
